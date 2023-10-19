652c084ba11e9d42507c823aeb97c66fc759bc61
LuisEGamez <luisgamezdvp@gmail.com>
2023-10-19 22:01:44 +0200

    frist commit

---
+++ src/test/kotlin/com/todoapi/TODOAPI/TodoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.todoapi.TODOAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
