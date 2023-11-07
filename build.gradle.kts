3b9964fc29ae2ba539215be4dba44c6ad1d45c90
Luis Gamez <luis.gamez@gantabi.com>
2023-11-08 00:59:03 +0100

    updating flyway pugling

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
-	id("org.flywaydb.flyway") version "9.1.6"
+	id("org.flywaydb.flyway") version "10.0.0"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 	kotlin("plugin.jpa") version "1.9.20"
